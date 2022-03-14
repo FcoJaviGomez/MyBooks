42c276ac4e81b2fcc1b3b0af84d7bd4a119ca02b
FcoJavierGómez <javiergomez742@gmail.com>
2022-03-14 17:41:26 +0100

    Subida dia1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormRegisterComponent } from './componet/form-register/form-register.component';
+import { HeaderComponent } from './componet/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
+import { RegisterComponent } from './pages/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: "", component: HomeComponent
+},
+{ path: "register", component: RegisterComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
