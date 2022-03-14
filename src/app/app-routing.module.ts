4d3f93ea57408a68d6bc092a9d3d82f3eb0d9ac9
FcoJavierGómez <javiergomez742@gmail.com>
2022-03-14 17:49:00 +0100

    Subida Main

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { FormRegisterComponent } from './componet/form-register/form-register.component';
-import { HeaderComponent } from './componet/header/header.component';
 import { HomeComponent } from './pages/home/home.component';
 import { RegisterComponent } from './pages/register/register.component';
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './componet/header/header.component';
-import { FooterComponent } from './componet/footer/footer.component';
+import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from './component/footer/footer.component';
 import { HomeComponent } from './pages/home/home.component';
-import { FormRegisterComponent } from './componet/form-register/form-register.component';
+import { FormRegisterComponent } from './component/form-register/form-register.component';
 import { RegisterComponent } from './pages/register/register.component';
 
 
